4ac648703442021e2e6075b2207c98035c3dfbd1
Hossain Rabbi <hossainrabbi259@gmail.com>
2021-06-29 08:39:22 +0600

    readme.md file added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header/Header';
-import Offers from '../components/Offers/Offers';
+// import Offers from '../components/Offers/Offers';
 import Products from '../components/Products/Products';
 import CartBtn from '../components/CartBtn/CartBtn';
 import Cart from '../components/Cart/Cart';
